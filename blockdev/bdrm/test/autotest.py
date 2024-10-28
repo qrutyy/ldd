ac719e59a761638896a6bf3e91fc2594acb057db
qrutyy <qrutyq@gmail.com>
2024-10-28 21:19:41 +0300

    test: fix typo

--- blockdev/bdrm/test/autotest.py
+++ blockdev/bdrm/test/autotest.py
@@ -43,7 +43,7 @@ def clean_dir(dir):
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
-              elif os.path.isdir(file_path):
+            elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
             print(f"Failed to delete {file_path}. Reason: {e}")
