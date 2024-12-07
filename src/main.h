e835bad518980ed230cf3cec1c98820bf75add7f
qrutyy <qrutyq@gmail.com>
2024-12-07 17:30:55 +0300

    feat: add sectors struct to decrease allocs

--- src/main.h
+++ src/main.h
@@ -2,16 +2,15 @@
 
 #pragma once
 
-#define MAX_BD_NAME_LENGTH 15
-#define MAX_MINORS_AM 20
-#define MAX_DS_NAME_LEN 2
-#define MAIN_BLKDEV_NAME "lsvbd"
-#define POOL_SIZE 50
-#define SECTOR_OFFSET 32
+#define LSBDD_MAX_BD_NAME_LENGTH 15
+#define LSBDD_MAX_MINORS_AM 20
+#define LSBDD_MAX_DS_NAME_LEN 2
+#define LSBDD_BLKDEV_NAME_PREFIX "lsvbd"
+#define LSBDD_SECTOR_OFFSET 32
 
 struct redir_sector_info {
-	sector_t *redirected_sector;
-	unsigned int block_size;
+	sector_t redirected_sector;
+	u32 block_size;
 };
 
 struct bdd_manager {
@@ -20,4 +19,9 @@ struct bdd_manager {
 	struct bdev_handle *bdev_handler;
 	struct data_struct *sel_data_struct;
 	struct list_head list;
+};
+
+struct sectors {
+	sector_t original;
+	sector_t redirect;
 };
