bf9a33bb378f462eeba2f60d23f999856ba8eb4a
qrutyy <qrutyq@gmail.com>
2024-12-18 18:33:43 +0300

    test: add more fio tests

--- test/fio/simple/ft_8_8.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-fio --name=fff --ioengine=io_uring --iodepth=16 --rw=write --size=1000M --bssplit=8k/100 --direct=1 --filename=/dev/lsvbd1 --numjobs=1 
-
-fio --name=fff --ioengine=io_uring --iodepth=16 --rw=read --size=1000M --bssplit=8k/100 --direct=1 --filename=/dev/lsvbd1 --numjobs=1 
--- test/fio/verify/ftv_8_8.sh
+++ test/fio/verify/ftv_8_8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Write test
-fio --name=fff --ioengine=io_uring --iodepth=16 --rw=write --size=100M --verify_state_save=1 --bssplit=8k/100 --direct=1 --filename=/dev/lsvbd1 --numjobs=1 --verify=pattern --verify_pattern=0xAA --do_verify=0 --verify_fatal=0 --verify_only=0 --runtime=20
+fio --name=fff --ioengine=libaio --iodepth=16 --rw=write --size=1000M --verify_state_save=1 --bssplit=8k/100 --direct=1 --filename=/dev/lsvbd1 --numjobs=1 --verify=pattern --verify_pattern=0xAA --do_verify=0 --verify_fatal=0 --verify_only=0 
 
 # Read and verify test
-fio --name=fff --ioengine=io_uring --iodepth=16 --rw=read --size=100M --verify_state_save=1 --bssplit=8k/100 --direct=1 --filename=/dev/lsvbd1 --numjobs=1 --verify=pattern --verify_pattern=0xAA --do_verify=0 --verify_fatal=0 --verify_only=1 --runtime=20
+fio --name=fff --ioengine=libaio --iodepth=16 --rw=read --size=1000M --verify_state_save=1 --bssplit=8k/100 --direct=1 --filename=/dev/lsvbd1 --numjobs=1 --verify=pattern --verify_pattern=0xAA --do_verify=0 --verify_fatal=0 --verify_only=1 
